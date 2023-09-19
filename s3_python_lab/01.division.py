d55670fc74e9190095e24d11db6fb41093cf014a
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2023-09-19 21:05:21 +0530

    edit diviion.py, odd_even.py

--- s3_python_lab/01.division.py
+++ s3_python_lab/01.division.py
@@ -1,14 +1,14 @@
 # 1. Find the quotient and remainder of two integers entered by user.
 
-dividend = int(input("Enter the dividend: "))
-divisor = int(input("Enter the divisor: "))
+dividend = int(input("Enter the dividend (integer): "))
+divisor = int(input("Enter the divisor (integer): "))
 
 print("The quotient is: ", dividend // divisor)
 print("And the remainder is: ", dividend % divisor)
 
 #Algorithm
 # 1. Prompt the user to enter the dividend and divisor and store them in a variable.
-# 2. Convert the dividend and divisor to an integer.
+# 2. Convert the dividend and divisor into integers.
 # 3. Calculate Quotient and Remainder:
 #    3.1. Quotient = dividend // divisor
 #    3.2. Remainder = dividend % divisor
--- s3_python_lab/02.odd_even.py
+++ s3_python_lab/02.odd_even.py
@@ -1,6 +1,6 @@
 # 2. Check whether an entered number is odd or even.
 
-num = int(input("Enter a number: "))
+num = int(input("Enter a integer: "))
 
 if (num % 2 == 0) : print("The number is even")
 else : print("The number is odd")
