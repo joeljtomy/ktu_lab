d750c89dd6fc326262ee11644b27426d485cf016
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2023-10-03 19:17:33 +0530

    minor edits prime_numbers.py

--- s3_python_lab/04.prime_numbers.py
+++ s3_python_lab/04.prime_numbers.py
@@ -15,17 +15,10 @@ for num in range(low, high + 1):
 # 1. Start
 # 2. Prompt the user to enter the low & high of a range and store them in two variables.
 # 3. Convert both of them to integers.
-# 4. create empty array prime_numbers to store prime numbers
-# 5. If low < 0 Display Low range should not be negative.
-# 6. Else continue to next steps
-# 7. Set i = low
-# 8. Repeat steps 7.1 to 7.4 for num in range(low, high + 1):
-# 8.1. Declare variables isPrime = True,
-# 8.2. Repeat steps 7.2.1 to 7.2.2 for x in range(2, int(math.sqrt(num)) + 1):
-# 8.2.1. If num % x == 0
-#        - set isPrime = False
+# 4. If low < 2 set low = 2
+# 5. Start loop for num in range(low, high + 1):
+# 5.1. Start loop for x in range(2, num // 2 + 1):
+# 5.1.1. If num % x == 0
 #        - break loop
-# 8.3. If isPrime append num to array prime_numbers
-# 9. If len(prime_numbers) Display Prime numbers
-# 10. Else Display No prime numbers in the range
-# 11. Stop
+# 5.2. Else : print(x, end = ", ")
+# 6. Stop
