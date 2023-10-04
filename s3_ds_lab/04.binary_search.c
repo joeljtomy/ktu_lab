fa02a3ad883927acc7adf93eae2815525706d76f
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2023-10-04 22:29:15 +0530

    minor fix remove sorting feature

--- s3_ds_lab/04.binary_search.c
+++ s3_ds_lab/04.binary_search.c
@@ -11,30 +11,12 @@ int main()
 
     int arr[n];
 
-    printf("Enter array elements:\n");
+    printf("Enter sorted array elements:\n");
     for (i = 0; i < n; i++)
     {
         printf("%d : ", i + 1);
         scanf("%d", &arr[i]);
     }
-
-    for (i = 0; i < n - 1; i++)
-    {
-        for (j = 0; j < n - i - 1; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-
-    printf("Sorted array: ");
-    for (i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
 
     printf("Enter the element to search: ");
     scanf("%d", &key);
