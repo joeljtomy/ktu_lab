6f7706acf84b84a801391a91c907fc36169e568c
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2023-10-06 21:04:30 +0530

    using count method of string

--- s3_python_lab/05.sub_str_count.py
+++ s3_python_lab/05.sub_str_count.py
@@ -2,18 +2,8 @@
 
 string = input("Enter a string: ").strip()
 sub_str = input("Enter the substring to count: ")
-word = ''
-count = 0
 
-for i in range(0, len(string)):
-    if string[i] != ' ':
-        word = word + string[i]
-    elif word != '':
-        if word == sub_str:
-            count = count + 1
-        word = ''
-if word == sub_str:
-    count = count + 1
+count = string.count(sub_str)
 
 print("Total {count} occurrences of '{sub_str}'".format(
     count=count, sub_str=sub_str))
@@ -23,15 +13,7 @@ print("Total {count} occurrences of '{sub_str}'".format(
 # 1. Start
 # 2. Prompt the user to enter a string and store it in a variable.
 # 3. Prompt the user to enter a sub_string and store it in another variable.
-# 4. Declare word = '', count = 0.
-# 5. Start loop for i in range(0, len(string)):
-# 5.1. If string[i] != ' ' :
-#        word = word + string[i]
-# 5.2  Elif word != '' :
-#        if word == sub_str:
-#          count = count + 1
-#        word = ''
-# 6. If word == sub_str :
-# 6.1. count = count + 1
-# 7. Print Total occurrences {count}
-# 8. Stop
+# 4. Declare count = 0.
+# 5. count = string.count(sub_str)
+# 6. Print Total occurrences {count}
+# 7. Stop
