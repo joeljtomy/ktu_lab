8aa019a2ffeba41bb335e5fafd8495c16c6a66dc
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2023-10-09 19:54:51 +0530

    remove count functionality

--- s3_ds_lab/03.linear_search.c
+++ s3_ds_lab/03.linear_search.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-    int i, n, key, count = 0;
+    int n, key, found = 0, index;
 
     printf("Enter the size of array: ");
     scanf("%d", &n);
@@ -12,7 +12,7 @@ int main()
     int arr[n];
 
     printf("Enter array elements:\n");
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         printf("%d : ", i + 1);
         scanf("%d", &arr[i]);
@@ -21,37 +21,39 @@ int main()
     printf("Enter the element to search: ");
     scanf("%d", &key);
 
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         if (arr[i] == key)
         {
-            printf("%d is found at index %d\n", key, i);
-            count++;
+            found = 1;
+            index = i;
+            break;
         }
     }
 
-    if (count)
-        printf("Total %d appearances of %d\n", count, key);
+    if (found)
+        printf("Element %d found at index %d\n", key, index);
     else
-        printf("%d is not in the array\n", key);
+        printf("Element %d not found in the array\n", key);
 
     return 0;
 }
 
 /*Algorithm
 Step 1: Start
-Step 2: Declare variables n, key and count = 0.
+Step 2: Declare variables n, i = 0, key, found and index.
 Step 3: Read n from the user.
 Step 4: Declare array arr with size n.
 Step 5: Read elements of arr from the user.
 Step 6: Read key from the user.
 Step 7: Repeat the steps until i < n
      7.1 If arr[i] == key
-            Display Element found at index 'i'.
-            count = count + 1
+            found ← 1
+            index ← i
+            Go to step 8
      7.2 i ← i+1
-Step 8: If count
-           Display count
+Step 8: If found
+           Display Element found
         else
            Display Element not found
 Step 9: Stop
