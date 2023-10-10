06f712bec54ae704935dd5d00ba0f1b44fb7b13a
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2023-10-10 19:07:04 +0530

    first commit

---
+++ s3_python_lab/07.search.py
@@ -0,0 +1,22 @@
+# 7.  Search an element in a list.
+
+user_list = input("Enter a list of numbers (comma-separated): ").strip(',').split(',')
+list = [item for item in user_list]
+
+key = input("Enter a search key: ")
+
+if key in list :
+    print("Search key found in the list.")
+else :
+    print("Search key is not in the list!")
+
+
+# Algorithm
+# 1. Start
+# 2. Prompt the user to enter a list and store it in a variable.
+# 3. Prompt the user to enter a search key and store it in a variable.
+# 4. If key in list :
+#        print("Search key found in the list.")
+# 5. Else :
+#        print("Search key is not in the list!")
+# 7. Stop
---
+++ s3_python_lab/08.squre_avg.py
@@ -0,0 +1,19 @@
+# 8.  Input a list of n numbers. Calculate and display the average of numbers. Also display
+#     the square of each value in the list.
+
+user_list = input("Enter a list of numbers (comma-separated): ").strip(',').split(',')
+list = [int(item) for item in user_list]
+
+for i in list:
+    print("Square of "+ str(i) +" = " + str(i * i))
+
+print("Average = " + str(sum(list) / len(list)))
+
+
+# Algorithm
+# 1. Start
+# 2. Prompt the user to enter a list and store it in a variable.
+# 3. for i in list:
+#        print("Square of "+ str(i) +" = " + str(i * i))
+# 5. print("Average = " + str(sum(list) / len(list)))
+# 7. Stop
