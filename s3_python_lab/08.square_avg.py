0355b9030966314b69ba447cf9c879b4c3a05a0d
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2023-10-16 19:53:28 +0530

    squares moved to list

--- s3_python_lab/08.square_avg.py
+++ s3_python_lab/08.square_avg.py
@@ -5,17 +5,18 @@ input_list = input(
     "Enter a list of numbers (comma-separated): ").strip(',').split(',')
 user_list = [int(item) for item in input_list]
 
-for i in user_list:
-    print("Square of", i, "=", i * i)
-
 print("Average =", sum(user_list) / len(user_list))
+
+sqr_list = [item * item for item in user_list]
+
+print("Square of elements:", sqr_list)
 
 
 # Algorithm
 # 1. Start
 # 2. Prompt the user to enter a list and store it in a variable.
 # 3. Convert the list items into integers
-# 4. Start loop for i in user_list:
-#        print("Square of", i, "=", i * i)
-# 5. print("Average =", sum(user_list) / len(user_list))
-# 6. Stop
+# 4. Print("Average =", sum(user_list) / len(user_list))
+# 5. sqr_list = [item * item for item in user_list]
+# 6. Print("Square of elements:", sqr_list)
+# 7. Stop
