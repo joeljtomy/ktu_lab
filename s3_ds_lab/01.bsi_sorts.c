8364d334b2b212cda8887e27d7ff71691b7584a6
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2023-11-01 22:32:19 +0530

    cleanup

--- s3_ds_lab/01.bsi_sorts.c
+++ s3_ds_lab/01.bsi_sorts.c
@@ -25,15 +25,15 @@ void selectionSort(int arr[], int n)
 {
     for (i = 0; i < n - 1; i++)
     {
-        int minIndex = i;
+        int smallest = i;
         for (j = i + 1; j < n; j++)
         {
-            if (arr[j] < arr[minIndex])
-                minIndex = j;
+            if (arr[j] < arr[smallest])
+                smallest = j;
         }
         int temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
+        arr[i] = arr[smallest];
+        arr[smallest] = temp;
     }
 }
 
