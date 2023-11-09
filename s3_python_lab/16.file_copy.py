2316b91349641fd59ee300fd440ee1eb82f33cc4
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2023-11-09 22:13:22 +0530

    minor edits

--- s3_python_lab/16.file_copy.py
+++ s3_python_lab/16.file_copy.py
@@ -1,19 +1,19 @@
 # 16. Develop a python code to read a text file, copy the contents to another file after
 #     removing the blank lines.
 
-with open("file_in.txt", "r") as file_in:
+file_path = input("Enter the file path: ")
+
+with open(file_path, "r") as file_in:
     content = file_in.read()
-
-print("file content:")
-print(content)
-
-with open("file_out.txt", "w") as file_out:
-    for line in content.split("\n"):
-        if line.strip() != "":
-            file_out.write(line+"\n")
+    print("file content:")
+    print(content)
+    with open("file_out.txt", "w") as file_out:
+        for line in content.split("\n"):
+            if line.strip() != "":
+                file_out.write(line+"\n")
 
 with open("file_out.txt", "r") as file_out:
-    print("file content after:")
+    print("file content after edit & copy:")
     print(file_out.read())
 
 
