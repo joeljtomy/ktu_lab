3671da7c37ea2a9d21021c550a115bba7ab8cc81
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2023-11-20 20:44:49 +0530

    minor edits

--- s3_ds_lab/12.postfix_eval.c
+++ s3_ds_lab/12.postfix_eval.c
@@ -2,74 +2,111 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-typedef struct Node {
-    int data;
-    struct Node *left;
-    struct Node *right;
-} Node;
+#define MAX_SIZE 100
 
-void inorderTraversal(Node *root) {
-    if (root == NULL) return;
-    inorderTraversal(root->left);
-    printf("%d, ", root->data);
-    inorderTraversal(root->right);
+int top = -1;
+int stack[MAX_SIZE];
+
+int empty() {
+    return top == -1;
 }
 
-void preorderTraversal(Node *root) {
-    if (root == NULL) return;
-    printf("%d, ", root->data);
-    preorderTraversal(root->left);
-    preorderTraversal(root->right);
+int full() {
+    return top == MAX_SIZE - 1;
 }
 
-void postorderTraversal(Node *root) {
-    if (root == NULL) return;
-    postorderTraversal(root->left);
-    postorderTraversal(root->right);
-    printf("%d, ", root->data);
+void push(char item) {
+    if (!full()) stack[++top] = item;
 }
 
-Node *createNode(int data) {
-    Node *newNode = malloc(sizeof(Node));
-    newNode->data = data;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    return newNode;
+int pop() {
+    if (!empty()) return stack[top--];
+    else return -1;
 }
 
-Node *insertLeft(Node *root, int data) {
-    root->left = createNode(data);
-    return root->left;
+int peek() {
+    if (!empty()) return stack[top];
+    else return -1;
 }
 
-Node *insertRight(Node *root, int data) {
-    root->right = createNode(data);
-    return root->right;
+int is_operator(char ch) {
+    return ch == '+' || ch == '-' || ch == '*' || ch == '/';
+}
+
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    else if (op == '*' || op == '/') return 2;
+    return 0;
+}
+
+void infix_to_postfix(char infix[], char postfix[]) {
+    int i, j;
+    char ch;
+
+    for (i = j = 0; infix[i] != '\0'; i++) {
+        ch = infix[i];
+
+        if (isalnum(ch)) postfix[j++] = ch;
+        else if (ch == '(') push(ch);
+        else if (ch == ')') {
+            while (peek() != '(' && !empty()) {
+                postfix[j++] = pop();
+            }
+            pop();
+        } else if (is_operator(ch)) {
+            while (!empty() && precedence(ch) <= precedence(peek())) {
+                postfix[j++] = pop();
+            }
+            push(ch);
+        }
+    }
+
+    while (!empty()) postfix[j++] = pop();
+    postfix[j] = '\0';
+}
+
+int postfix_evaluation(char postfix[]) {
+    int i, operand1, operand2, result;
+    char ch;
+
+    for (i = 0; postfix[i] != '\0'; i++) {
+        ch = postfix[i];
+
+        if (isdigit(ch))  push(ch - '0');
+        else if (is_operator(ch)) {
+            operand2 = pop();
+            operand1 = pop();
+
+            switch (ch) {
+                case '+': push(operand1 + operand2); break;
+                case '-': push(operand1 - operand2); break;
+                case '*': push(operand1 * operand2); break;
+                case '/': push(operand1 / operand2); break;
+            }
+        }
+    }
+
+    result = pop();
+    return result;
 }
 
 int main() {
-    int data;
-    printf("Enter the data of root node: ");
-    scanf("%d", &data);
-    Node *root = createNode(data);
-    printf("Enter the data of left node: ");
-    scanf("%d", &data);
-    insertLeft(root, data);
-    printf("Enter the data of right node: ");
-    scanf("%d", &data);
-    insertRight(root, data);
+    char infix[MAX_SIZE], postfix[MAX_SIZE];
 
-    printf("\nInorder traversal: ");
-    inorderTraversal(root);
+    printf("Enter infix expression: ");
+    scanf("%s", infix);
 
-    printf("\nPreorder traversal: ");
-    preorderTraversal(root);
+    infix_to_postfix(infix, postfix);
+    printf("\nPostfix expression: %s\n", postfix);
 
-    printf("\nPostorder traversal: ");
-    postorderTraversal(root);
+    int result = postfix_evaluation(postfix);
+    printf("Result after evaluation: %d\n", result);
+
     return 0;
 }
+
 
 /*Algorithm
 Step 1: Start.
