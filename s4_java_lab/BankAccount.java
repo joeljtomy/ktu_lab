9a900d3bdcf1844371d1ee7271ff3ffc86053aec
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2024-02-15 19:37:17 +0530

    cleanup

---
+++ s4_java_lab/BankAccount.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BankAccount {
+    private String number;
+    private String holderName;
+    private double balance;
+
+    public void addAccountDetails() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Account details: ");
+        this.number = input.nextLine();
+        this.holderName = input.nextLine();
+        this.balance = input.nextDouble();
+    }
+
+    public void displayAccountDetails() {
+        System.out.println("Account Number: " + number);
+        System.out.println("Account Holder Name: " + holderName);
+        System.out.println("Account Balance: $" + balance);
+    }
+
+    public static void main(String[] args) {
+        BankAccount myAccount = new BankAccount();
+        myAccount.addAccountDetails();
+        myAccount.displayAccountDetails();
+    }
+}
