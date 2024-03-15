0c73db5a395c65e8bdc6aad47f263a71c4af0f1d
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2024-03-15 21:29:15 +0530

    minor edits

---
+++ s4_dbms_lab/ddl_1.sql
@@ -0,0 +1,30 @@
+CREATE TABLE student (
+    roll_no INT PRIMARY KEY,
+    name VARCHAR(50),
+    address VARCHAR(100),
+    phone_no VARCHAR(15),
+    gender VARCHAR(10),
+    branch VARCHAR(50),
+    mark1 INT,
+    mark2 INT
+);
+
+DESC student;
+
+ALTER TABLE student
+ADD total_marks INT;
+
+ALTER TABLE student
+MODIFY address VARCHAR(20);
+
+ALTER TABLE student
+DROP COLUMN gender,
+DROP COLUMN branch;
+
+ALTER TABLE student
+RENAME COLUMN mark1 TO m1;
+
+ALTER TABLE student
+RENAME TO student1;
+
+DROP TABLE student1;
