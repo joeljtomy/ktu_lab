82ffeb2fb0104161713bbc60cf3aeac55adf0ac1
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2024-03-15 21:03:06 +0530

    minor edits

--- s4_java_lab/BankAccount.java
+++ s4_java_lab/BankAccount.java
@@ -9,12 +9,17 @@ public class BankAccount {
     public void addAccountDetails() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter Account details: ");
+        System.out.print("Enter Account number: ");
         this.number = input.nextLine();
+        System.out.print("Enter Account holder name: ");
         this.holderName = input.nextLine();
+        System.out.print("Enter balance: ");
         this.balance = input.nextDouble();
     }
 
     public void displayAccountDetails() {
+        System.out.println();
+        System.out.println("Account details: ");
         System.out.println("Account Number: " + number);
         System.out.println("Account Holder Name: " + holderName);
         System.out.println("Account Balance: $" + balance);
