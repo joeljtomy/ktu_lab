3d4523cf3f7462015079ee4d5d00ceddbe6c8d1a
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2024-03-21 19:17:10 +0530

    cleanup

--- s4_dbms_lab/dml.sql
+++ s4_dbms_lab/dml.sql
@@ -1,8 +1,8 @@
 -- 1. Create a Student table with the following fields Name, Roll no, Age, Branch
 CREATE TABLE Student (
     Name VARCHAR(100),
-    Roll_no INT PRIMARY KEY,
-    Age INT,
+    Roll_no NUMBER PRIMARY KEY,
+    Age NUMBER,
     Branch VARCHAR(50)
 );
 
@@ -10,33 +10,34 @@ INSERT INTO Student VALUES ('&Name', &Roll_no, &Age, '&Branch');
 
 -- 2. Create table called ‘Distributor’ with columns Dcode, Codename, Amount, Limit
 CREATE TABLE Distributor (
-    Dcode INT PRIMARY KEY,
+    Dcode NUMBER PRIMARY KEY,
     Codename VARCHAR(50),
-    Amount INT(20),
-    Limit INT(20)
+    Amount NUMBER(20),
+    Limit NUMBER(20)
 );
 
 INSERT INTO Distributor VALUES (&Dcode, '&Codename', &Amount, &Limit);
 
 -- 3. Create a table called ‘Agencies’ with columns Agcode, Agname, Amount, Aglimit 
 CREATE TABLE Agencies (
-    Agcode INT PRIMARY KEY,
+    Agcode NUMBER PRIMARY KEY,
     Agname VARCHAR(50),
-    Amount INT(20),
-    Aglimit INT(20)
+    Amount NUMBER(20),
+    Aglimit NUMBER(20)
 );
+
 -- 4. 
 INSERT INTO Agencies VALUES (&Agcode, '&Agname', &Amount, &Aglimit);
 
--- 5. Alter the structure of table student by adding a column called Totmarks
---    and insert values into the added field.
+-- 5. Alter the structure of table student by adding a column called Totmarks and insert values into the added field.
 ALTER TABLE student
-ADD Totmarks INT;
+ADD Totmarks NUMBER;
 
-INSERT INTO Student VALUES &Totmarks;
+UPDATE student
+SET Totmarks = &Totmarks
+WHERE Roll_no = &Roll_no;
 
--- 6. Update the ‘Agencies table, set amount field to 5000 corresponding to 
---    Agcode = 403.
+-- 6. Update the ‘Agencies table, set amount field to 5000 corresponding to Agcode = 403.
 UPDATE Agencies
 SET Amount = 5000
 WHERE Agcode = 403;
@@ -45,8 +46,7 @@ WHERE Agcode = 403;
 DELETE FROM Distributor
 WHERE Amount < 70000;
 
--- 8. Display the Agcode and Agname from table Agencies in reverse order of 
---    their amount.
+-- 8. Display the Agcode and Agname from table Agencies in reverse order of their amount.
 SELECT Agcode, Agname
 FROM Agencies
 ORDER BY Amount DESC;
@@ -62,6 +62,8 @@ SELECT Name AS studentname, Roll_no AS student_rollno, Age, Branch, Totmarks
 FROM student;
 
 -- 11. Display the student info only in CS Branch.
+INSERT INTO Student VALUES ('&Name', &Roll_no, &Age, '&Branch', &Totmarks);
+
 SELECT * 
 FROM Student
 WHERE Branch = 'CS';
